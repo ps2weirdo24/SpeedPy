1287ed9f67cdc2a8bbca77394c812b0c61a244ed
ryangaudy <ryangaudy@yahoo.com>
2017-01-04 11:18:37 -0800

    Typo fixed and gitignore updated
    .gitignore now includes .txt files to remove requirements.txt and
    log.txt

--- speedmain.py
+++ speedmain.py
@@ -1,7 +1,6 @@
 # DOWN = 11-15
 # UP = 2-3
-# 
-# 
+
 
 import pyspeedtest
 import arrow
@@ -9,24 +8,51 @@ import time
 import schedule
 import json
 
+
 st = pyspeedtest.SpeedTest()
 TO_CONNECT = "https://www.google.com/"
 DOWN_MIN = 11
 DOWN_MAX = 15
 UP_MIN = 2
 UP_MAX = 3 
-ALLOWANCE = .7
+ALLOWANCE = .5
+IS_OVER_ALLOWANCE = None
+
+
+def data_to_str(the_float, dec_allow):
+	if (the_float >= dec_allow):
+		nice_percent = "%.2f" % (the_float*100)+"%"
+		return(nice_percent)
+	else:
+		nice_percent = "%.2f" % (the_float*100)+"% ***"
+		return(nice_percent)
 
 def record_results(conn_obj):
 	right_now = arrow.now("US/Pacific").format('YYYY-MM-DD HH:mm:ss')
 	if conn_obj["conn"]:
+		#if ((DOWN_MIN+DOWN_MAX)/2.0) >= ALLOWANCE
+		percents = {"down_low": (float(conn_obj["down"])/DOWN_MIN),
+					"down_middle": (float(conn_obj["down"])/((DOWN_MIN+DOWN_MAX)/2.0)),
+					"up_low": (float(conn_obj["up"])/UP_MIN),
+					"up_middle": (float(conn_obj["up"])/((UP_MIN+UP_MAX)/2.0))}
 		to_write = "\n%s, %s, %s, %s, %s" % (right_now, str(conn_obj["conn"]),
 									   str(conn_obj["down"]), str(conn_obj["up"]),
 									   str(conn_obj["ping"]))
+		second_write = "\n\t%s, %s, %s, %s, %s" % (str(ALLOWANCE),data_to_str(percents["down_low"],ALLOWANCE),
+											   data_to_str(percents["down_middle"],ALLOWANCE),
+											   data_to_str(percents["up_low"],ALLOWANCE),
+											   data_to_str(percents["up_middle"],ALLOWANCE))
 		txt_log = open("log.txt","a")
+		txt_log.write((to_write+second_write))
+		txt_log.close()
+		print((to_write+second_write))
+	else:
+		to_write = "\n==========%s===Can't Connect==========" % (right_now)
+		txt_log = open("log.txt", "a")
 		txt_log.write(to_write)
 		txt_log.close()
 		print(to_write)
+
 def connect_to(dest):
 	connectionAlive = True
 	the_down = 0
@@ -35,6 +61,8 @@ def connect_to(dest):
 	to_return = None	
 	try:
 		the_ping = st.ping()
+		the_down = st.download()
+		the_up = st.upload()
 	except:
 		connectionAlive = False
 		return({"conn": False,"ping": 0,"down": 0,"up": 0})
@@ -42,8 +70,6 @@ def connect_to(dest):
 	#print("Connected: %s" % (str(connectionAlive)))
 
 	if connectionAlive:
-		the_down = st.download()
-		the_up = st.upload()
 		the_ping = str(int(the_ping))
 		the_down = ("%.2f" % (the_down/1000000))
 		the_up = ("%.2f" % (the_up/1000000))
@@ -54,8 +80,6 @@ def connect_to(dest):
 
 def main():
 	record_results(connect_to(TO_CONNECT))
-
-
 
 
 if __name__ == "__main__":
